7b17d943ac1d385f9773fa3603960c7e1e0a631e
andrespaolo94 <71106497+andrespaolo94@users.noreply.github.com>
2020-09-11 11:55:42 -0500

    Update Untitled-1.py

--- static/js/Untitled-1.py
+++ static/js/Untitled-1.py
@@ -3,21 +3,23 @@ import time
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
-b1=4
-b2=5
-l1=5
-l2=13
+b1=12
+b2=13
+l1=7
+l2=11
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
+CrearFile=open("Misdatos.dat","w")
+CrearFile.write("Bienbenidos")
 
 
 def main():
     mqttc=mqtt.Client()
     mqttc.on_message=on_message
-    mqttc.username_pw_set("paoolo_9419@hotmail.com","@ndrespaolo")
+    mqttc.username_pw_set("jomsk@hotmail.com","Jomsk4all1996")
     mqttc.connect("maqiatto.com",1883)
-    mqttc.subscribe("paoolo_9419@hotmail.com/IoT1",0)
+    mqttc.subscribe("jomsk@hotmail.com/IoT1",0)
     FechaActual=" "
     GPIO.setup(b2,GPIO.IN)
     GPIO.setup(l2,GPIO.OUT)
@@ -27,15 +29,15 @@ def main():
     
     while(1):
         tiempo=datetime.now()
-        CrearFile=open("informacion.txt","a")
+        CrearFile=open("Misdatos.dat","a")
         mqttc.loop_start()
         FechaActual=str(tiempo)
         if(GPIO.input(b2)==0):
-            CrearFile.write("Se Ha pulsado 2")
+            CrearFile.write(FechaActual+"Se Ha pulsado 2")
             GPIO.output(l2,1)
-            mqttc.publish("paoolo_9419@hotmail.com/IoT","L;H")
-            time.sleep(0.5)
-            mqttc.publish("paoolo_9419@hotmail.com/IoT","L;L")
+            mqttc.publish("jomsk@hotmail.com/IoT","L;H")
+            time.sleep(1)
+            mqttc.publish("jomsk@hotmail.com/IoT","L;L")
             GPIO.output(l2,0)
             CrearFile.close()
             
@@ -45,21 +47,10 @@ def main():
             CrearFile.write(FechaActual+" Se Ha pulsado 1 \n")
             
             GPIO.output(l1,1)
-            mqttc.publish("paoolo_9419@hotmail.com/IoT","H;L")
-            time.sleep(0.5)
-            mqttc.publish("paoolo_9419@hotmail.com/IoT","L;L")
+            mqttc.publish("jomsk@hotmail.com/IoT","H;L")
+            time.sleep(1)
+            mqttc.publish("jomsk@hotmail.com/IoT","L;L")
             GPIO.output(l1,0)
-            CrearFile.close()
 
-        
-        
-        
-        
-        
-        time.sleep(2)
-        
-        
-            
 def on_message(client,obj,msg):
     print(msg.topic+" "+str(msg.qos)+" "+msg.payload.decode('utf-8'))
-            
